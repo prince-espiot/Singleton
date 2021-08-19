967f45bc13d309e871c3fe2d38fded7b6642e39d
Prince Okumo <prokum@ttu.ee>
2021-08-19 10:46:40 +0300

    fisrt com

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: admin
+ * Date: 8/19/2021
+ * Time: 10:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace singleton
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
